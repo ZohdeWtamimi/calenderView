c54421180faec1268b463758b2c6e04e88c8619a
ZohdeWtamimi <115448717+ZohdeWtamimi@users.noreply.github.com>
2023-11-10 06:03:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log('hello world')
 var isMousedown = false;
 var parentId = '';
 var selectedTimeSlots = {}; // New variable to store selected time slots
+var selectedTimeAllWeek = {}; // New variable to store selected time slots
 
 const days = document.querySelectorAll('.day-container');
 const hours = document.querySelectorAll('.day-container div:not(.hour):not(.head)');
@@ -13,11 +14,11 @@ days.forEach((day) => {
         parentId = getParentId(e.target.parentNode.classList.value);
         e.target.className = setClass(parentId);
         isMousedown = true;
+        getSelectedHours()
     });
 
     day.addEventListener('mouseup', (e) => { 
         isMousedown = false;
-        displaySelectedTimeSlots(parentId); // Call the function to display selected time slots
     });
 });
 
@@ -28,6 +29,7 @@ hours.forEach((hour) => {
         if (isMousedown && isSameDay) {
             e.target.className = setClass(parentId);
             const timeSlot = e.target.innerText;
+            getSelectedHours()
         }
     });
 
@@ -47,10 +49,50 @@ function setClass(id) {
     return `${id}-selected`;
 }
 
-function displaySelectedTimeSlots(day) {
-    console.log("Selected Time Slots:");
-    
-    ele = document.querySelector(`.${day}.day-container`);
-    console.log(ele.children)
+
+function getSelectedHours() {
+    daysArray.forEach(day => {
+        let eles = document.querySelectorAll(`.${day}-selected`);
+        if (eles.length > 0) {
+            let from = eles[0].getAttribute('data-time');
+            let to = eles[eles.length - 1].getAttribute('data-time');
+            selectedTimeAllWeek[day] = addOneHourToToTime({ from: from, to: to });
+        }
+    });
+    console.log(selectedTimeAllWeek);
+}
+
+
+
+function addOneHourToToTime(timeObject) {
+    // Extract hours
+    let fromHours = parseInt(timeObject.from);
+    let toHours = parseInt(timeObject.to);
+    let duration = (toHours + 1) - fromHours;
+
+    // fromHours = parseInt(fromHours) >= 10 ? fromHours : '0' + fromHours
+    let to = parseInt(fromHours) + duration
+
+    if (to == 12) {
+        to = '12:00 PM';
+    } else if (to == 24) {
+        to = '12:00 AM';
+    } else if (to > 12) {
+        to = (to - 12) + ':00 PM';
+    } else {
+        to = to + ':00 AM';
+    }    
+
+    if (fromHours == 12) {
+        fromHours = '12:00 PM';
+    } else if (fromHours > 12) {
+        fromHours = (fromHours - 12) + ':00 PM';
+    } else {
+        fromHours = fromHours + ':00 AM';
+    }    
+
+    console.log(`from ${fromHours } to ${ to } duration ${duration}` )
+
+    return {from: fromHours, to}
 
 }
